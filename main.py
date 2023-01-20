2d355fe5ee635cefb1430fbd6b0446f5a436ab47
cajewell01 <99988923+cajewell01@users.noreply.github.com>
2023-01-20 04:44:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+name = input("What is your name?\n")
+print("Hello " + name + "!")
+city = input(name + ", what is the name of the city you grew up in?\n")
+pet_name = input(name + ", what is the name of your pet?\n")
+print("Your band name should be " + city + " " + pet_name)
