45e88cd9911a4f835bf6f735bb1081ae5f351a28
cajewell01 <99988923+cajewell01@users.noreply.github.com>
2023-01-21 00:58:03 -0600

    Update tip_calculator.py

--- tip_calculator.py
+++ tip_calculator.py
@@ -12,3 +12,15 @@ splitTotal = format(splitTotal, '.2f')
 print(f"Each person should pay: ${splitTotal}")
 
 #The '.2f' is what formats the result to two decimal places. 
+
+
+#This code below is not my own, it belongs to the instructor. I saved it for learning purposes.
+print("Welcome to the Tip Calculator!")
+total = float(input("What was the total of the bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+bill_with_tip = tip / 100 * bill + bill
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
+print(f"Each person should pay: ${final_amount}")
