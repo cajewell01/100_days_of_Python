aaeb89dd7c7e411c4ad8be065ae2f76efbba769f
cajewell01 <99988923+cajewell01@users.noreply.github.com>
2023-02-08 19:53:29 -0600

    Update rock_paper_scissors.py

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -27,43 +27,47 @@ scissors = '''
 
 #Write your code below this line 👇
 import random
-
+game_images = [rock, paper, scissors]
 print("Welcome to Rock, Paper, Scissors!")
 
 personal_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.  "))
+print("\nYour Choice: ")
+if personal_choice >= 3 or personal_choice < 0:
+    print("You typed an invalid number, you lose!")
+else:
+    print(game_images[personal_choice])
 
-print("\n Your Choice: ")
-if personal_choice == 0:
-  print(rock)
-elif personal_choice == 1:
-  print(paper)
-elif personal_choice == 2:
-  print(scissors)
+    print("\nComputer Choice:")
+    computer_choice = random.randint(0, 2)
+    print(game_images[computer_choice])
 
-print("\nComputer Choice:")
-computer_choice = random.randint(0, 2)
-if computer_choice == 0:
-  print(rock)
-elif computer_choice == 1:
-  print(paper)
-elif computer_choice == 2:
-  print(scissors)
+    if personal_choice == 0 and computer_choice == 0:
+        print("It's a Draw.")
+    elif personal_choice == 0 and computer_choice == 1:
+        print("You Lose.")
+    elif personal_choice == 0 and computer_choice == 2:
+        print("You Win!")
+    elif personal_choice == 1 and computer_choice == 0:
+        print("You Win!")
+    elif personal_choice == 1 and computer_choice == 1:
+        print("It's a Draw.")
+    elif personal_choice == 1 and computer_choice == 2:
+        print("You Lose.")  
+    elif personal_choice == 2 and computer_choice == 0:
+        print("You Lose.")
+    elif personal_choice == 2 and computer_choice == 1:
+        print("You Win!")
+    elif personal_choice == 2 and computer_choice == 2:
+        print("It's a Draw.") 
 
-if personal_choice == 0 and computer_choice == 0:
-  print("It's a Draw.")
-elif personal_choice == 0 and computer_choice == 1:
-  print("You Lose.")
-elif personal_choice == 0 and computer_choice == 2:
-  print("You Win!")
-elif personal_choice == 1 and computer_choice == 0:
-  print("You Win!")
-elif personal_choice == 1 and computer_choice == 1:
-  print("It's a Draw.")
-elif personal_choice == 1 and computer_choice == 2:
-  print("You Lose.")  
-elif personal_choice == 2 and computer_choice == 0:
-  print("You Lose.")
-elif personal_choice == 2 and computer_choice == 1:
-  print("You Win!")
-elif personal_choice == 2 and computer_choice == 2:
-  print("It's a Draw.") 
+#This is how the instructor did her code. I wanted to save it because the logic is less code than mine
+    #if personal_choice >= 3 or personal_choice < 0:
+        #print("You typed an invalid number, you lose!")
+    #elif personal_choice == 0 and computer_choice == 2:
+        #print("You Win!")
+    #elif computer_choice == 0 and personal_choice ==2:
+        #print("You Lose.")
+    #elif computer_choice > personal_choice:
+        #print("You Lose.")
+    #elif personal_choice > computer_choice:
+        #print("You Win!")
